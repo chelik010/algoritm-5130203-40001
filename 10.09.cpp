1d1cec5857df76e3af43c1dbd7a450638f5cad2c
chelik010 <chelik12@protonmail.com>
2024-09-10 14:50:30 +0300

    init

---
+++ 10.09.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b, g;
+    float f;
+    cout << "Введите A: ";
+    cin >> a;
+    cout << "Введите B: ";
+    cin >> b;
+
+    if (a < b)
+    {
+        f = sqrt(a) * b;
+    }
+    else if (a > b && b < 0)
+    {
+        f = pow(a, 2) + b;
+    }
+    else if (a > b && b >= 0)
+    {
+        f = a - b;
+    }
+    cout << "F = " << f << endl;
+
+    if (a <=1)
+    {
+        g=0;
+    }
+}
